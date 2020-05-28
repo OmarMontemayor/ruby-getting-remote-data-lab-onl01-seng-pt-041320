d1e8529f7dc9913e8250ffc19049f07e47cf7d42
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-05-28 16:17:09 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,23 +1,31 @@
-# Write your code here
 require 'net/http'
 require 'open-uri'
 require 'json'
 require 'pry'
+
 class GetRequester
+
+    def initialize(url = "https://swapi.dev/api/films/")
+        @url = url
+    end
   
-  def initialize(url)
-    @url = url
-  end
+    def get_response_body
+        #return body of response 
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
   
-  def get_response_body
-  #return body of response 
-    uri = URI.parse(@url)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end
-  
-  def parse_json
-    data = JSON.parse(self.get_response_body)
-    data
-  end
+    def parse_json
+        data = JSON.parse(self.get_response_body)
+        data
+    end 
+    
+    def film_creator
+      self.get_response_body.parse_json.each do |title, episode_id|
+            binding.pry
+            Film.new(title,episode_id)
+        end
+    end
 end
+
